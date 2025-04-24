79cdf09dd9217da8337a68381fe0f91e03be8952
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 20:15:33 +0900

    chore: SearchAndFilter 컴포넌트에서 필터 분리

---
+++ src/components/Search.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+
+import { SearchIcon } from "lucide-react";
+
+export const Search = () => {
+  return (
+    <div className="flex w-full gap-2">
+      <div className="relative flex flex-1 items-center">
+        <Input className="bg-element w-full !p-2" />
+        <Button
+          variant="icon"
+          className="absolute right-1"
+        >
+          <SearchIcon className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
+};
