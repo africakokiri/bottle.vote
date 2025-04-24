294a4387cbacf30599a98326818db24b9b76c06b
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 17:28:27 +0900

    chore: Init prettier, eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    plugins: ["only-warn", "unused-imports"],
+    rules: {
+      "unused-imports/no-unused-imports": "warn",
+      "@typescript-eslint/consistent-type-imports": "warn"
+    }
+  })
 ];
 
 export default eslintConfig;
