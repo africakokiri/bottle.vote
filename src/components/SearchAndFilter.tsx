dea849b3a081b26ee84ef550c1e4c7a0c589d3af
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 20:02:24 +0900

    style: SearchAndFilter 컴포넌트 스타일 수정

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -11,7 +11,7 @@ export const SearchAndFilter = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex w-full gap-4">
+    <div className="flex w-full gap-2">
       <div className="relative flex flex-1 items-center">
         <Input className="bg-element w-full !p-2" />
         <Button
