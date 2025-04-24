083d542124bdea79d602ccdf3832dd3ae7f7fbc4
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 20:38:24 +0900

    chore: Search 컴포넌트 폼 형태로 변경

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,15 +8,16 @@ import { SearchIcon } from "lucide-react";
 export const Search = () => {
   return (
     <div className="flex w-full gap-2">
-      <div className="relative flex flex-1 items-center">
+      <form className="relative flex flex-1 items-center">
         <Input className="bg-element w-full !p-2" />
         <Button
           variant="icon"
+          type="submit"
           className="absolute right-1 !p-2"
         >
           <SearchIcon className="h-4 w-4" />
         </Button>
-      </div>
+      </form>
     </div>
   );
 };
