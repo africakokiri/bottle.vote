5846b96275400267fbfced43dc3c98d2c60e03cc
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 20:32:46 +0900

    style: Input UI 컴포넌트 스타일 수정

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -9,7 +9,7 @@ export const Input = ({ className, ref, ...props }: InputProps) => {
   return (
     <input
       className={`${className} focus-within:ring-offset-element rounded-md border px-3 py-1.5
-ring-offset-2 transition outline-none hover:bg-neutral-100 focus-visible:ring-2
+ring-offset-2 transition outline-none hover:border-[1px] hover:border-fuchsia-500 focus-visible:ring-2
 focus-visible:ring-fuchsia-500`}
       ref={ref}
       {...props}
