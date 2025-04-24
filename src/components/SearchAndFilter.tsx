4715887c6ed2eb99e266abbc4adf7c29dbba590f
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 19:50:56 +0900

    chore: SearchAndFilter 컴포넌트 추가

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import { Modal } from "@/components/ui/Modal";
 
 import { FilterIcon, SearchIcon } from "lucide-react";
+import { useState } from "react";
 
 export const SearchAndFilter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex w-full gap-4">
       <div className="relative flex flex-1 items-center">
@@ -19,12 +25,15 @@ export const SearchAndFilter = () => {
       <Button
         variant="icon"
         className="border"
+        onClick={() => setIsOpen(true)}
       >
         <FilterIcon
           className="h-4 w-4"
           strokeWidth={1.5}
         />
       </Button>
+
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
     </div>
   );
 };
