f2fe0f4593ea6363c33f9edac808e87acc20cea4
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 19:17:12 +0900

    chore: 카테고리 배열 const 파일 추가

---
+++ src/const/const.ts
@@ -0,0 +1,15 @@
+export const categories = [
+  { id: "all", name: "모든 카테고리" },
+  { id: "technology", name: "기술" },
+  { id: "entertainment", name: "엔터테인먼트" },
+  { id: "sports", name: "스포츠" },
+  { id: "politics", name: "정치" },
+  { id: "food", name: "음식" },
+  { id: "travel", name: "여행" },
+  { id: "gaming", name: "게임" },
+  { id: "music", name: "음악" },
+  { id: "education", name: "교육" },
+  { id: "business", name: "비즈니스" },
+  { id: "science", name: "과학" },
+  { id: "health", name: "건강" }
+];
