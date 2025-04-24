7451b298b1e3931eafb50e805bdd8fea710f42c6
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-24 20:28:56 +0900

    style: Input UI 컴포넌트 스타일 수정

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -8,8 +8,9 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
 export const Input = ({ className, ref, ...props }: InputProps) => {
   return (
     <input
-      className={`${className} focus-within:ring-offset-background rounded-md border px-3 py-1.5
-ring-offset-2 transition outline-none focus-visible:ring-2 focus-visible:ring-black`}
+      className={`${className} focus-within:ring-offset-element rounded-md border px-3 py-1.5
+ring-offset-2 transition outline-none hover:bg-neutral-100 focus-visible:ring-2
+focus-visible:ring-fuchsia-500`}
       ref={ref}
       {...props}
     />
