70c43f5298a517f9020610b993214ca075d3e4c2
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-25 11:44:05 +0900

    chore: 기본적인 /vote/list 페이지 UI 구성

--- src/app/vote/list/page.tsx
+++ src/app/vote/list/page.tsx
@@ -1,14 +1,20 @@
-import { SearchAndFilter } from "@/components/SearchAndFilter";
+import { Search } from "@/components/Search";
+import { SelectAndFilter } from "@/components/SelectAndFilter";
 
 export default function page() {
   return (
-    <div className="container space-y-4">
+    <div className="container max-w-7xl space-y-4">
       <div>
-        <h1 className="text-2xl font-bold">투표 목록</h1>
+        <h1 className="text-3xl font-bold">투표 목록</h1>
       </div>
 
-      {/* 검색 및 필터 */}
-      <SearchAndFilter />
+      <div className="gap-4 space-y-4 sm:flex">
+        {/* 검색  */}
+        <Search />
+
+        {/* 모든 투표 / 인기투표 */}
+        <SelectAndFilter />
+      </div>
     </div>
   );
 }
