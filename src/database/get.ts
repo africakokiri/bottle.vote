8543c96afc949553dd936ba405cde6a47ca1e138
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-25 13:48:04 +0900

    chore: Prisma getPopularVotes 함수 추가

--- src/database/get.ts
+++ src/database/get.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getAllPosts = async () => {
+export const getAllVotes = async () => {
   const allPosts = await prisma.votes.findMany({
     include: {
       users: true,
@@ -13,4 +13,19 @@ export const getAllPosts = async () => {
   });
 
   return allPosts;
+};
+
+export const getPopularVotes = async (howMany: number) => {
+  const popularVotes = await prisma.votes.findMany({
+    include: {
+      users: true,
+      vote_options: true
+    },
+    orderBy: {
+      vote_count: "desc"
+    },
+    take: howMany
+  });
+
+  return popularVotes;
 };
