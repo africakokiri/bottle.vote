15a3c6face3f5259b8b9f26c04280a4964fe5f74
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-25 13:41:04 +0900

    chore: Prisma getAllPosts 함수 수정

--- src/database/get.ts
+++ src/database/get.ts
@@ -5,7 +5,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getAllPosts = async () => {
-  const allPosts = await prisma.votes.findMany();
+  const allPosts = await prisma.votes.findMany({
+    include: {
+      users: true,
+      vote_options: true
+    }
+  });
 
   return allPosts;
 };
