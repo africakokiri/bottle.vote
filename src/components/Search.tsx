86af4d9a5d15bf177a7e9865a3c797f665376eeb
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-25 15:33:07 +0900

    chore: Search 컴포넌트 수정

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,14 +2,40 @@
 
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import { getVotesLength } from "@/database/get";
 
 import { SearchIcon } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
 
 export const Search = () => {
+  const [votesLength, setVotesLength] = useState({
+    allVotesLength: 0,
+    activeVotesLength: 0
+  });
+
+  const fetchVotesLength = useCallback(async () => {
+    const data = await getVotesLength();
+    setVotesLength({
+      allVotesLength: Number(data.allVotesLength),
+      activeVotesLength: Number(data.activeVotesLength)
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchVotesLength();
+  }, [fetchVotesLength]);
+
   return (
     <div className="flex w-full gap-2">
       <form className="relative flex flex-1 items-center">
-        <Input className="bg-element w-full !p-2" />
+        <Input
+          placeholder={
+            votesLength.activeVotesLength
+              ? `총 ${votesLength.allVotesLength}개의 투표와 ${votesLength.activeVotesLength}개의 진행중인 투표`
+              : "..."
+          }
+          className="bg-element w-full !p-2"
+        />
         <Button
           variant="icon"
           type="submit"
