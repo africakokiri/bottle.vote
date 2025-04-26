4db0f412cd72813633582e151614eab7f615d068
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-26 10:55:26 +0900

    chore: Const categories 배열 수정 및 타입 추가

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,15 +1,20 @@
 export const categories = [
-  { id: "all", name: "모든 카테고리" },
-  { id: "technology", name: "기술" },
-  { id: "entertainment", name: "엔터테인먼트" },
-  { id: "sports", name: "스포츠" },
-  { id: "politics", name: "정치" },
-  { id: "food", name: "음식" },
-  { id: "travel", name: "여행" },
-  { id: "gaming", name: "게임" },
-  { id: "music", name: "음악" },
-  { id: "education", name: "교육" },
-  { id: "business", name: "비즈니스" },
-  { id: "science", name: "과학" },
-  { id: "health", name: "건강" }
-];
+  { value: "technology", name: "기술" },
+  { value: "entertainment", name: "엔터테인먼트" },
+  { value: "sports", name: "스포츠" },
+  { value: "politics", name: "정치" },
+  { value: "food", name: "음식" },
+  { value: "travel", name: "여행" },
+  { value: "gaming", name: "게임" },
+  { value: "music", name: "음악" },
+  { value: "education", name: "교육" },
+  { value: "business", name: "비즈니스" },
+  { value: "science", name: "과학" },
+  { value: "health", name: "건강" },
+  { value: "life", name: "생활" },
+  { value: "others", name: "기타" }
+] as const;
+
+export type Categories = (typeof categories)[number];
+export type CategoriesValue = (typeof categories)[number]["value"];
+export type CategoriesName = (typeof categories)[number]["name"];
