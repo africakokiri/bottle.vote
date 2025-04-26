4946dc4a614862360b7f459e387523e3e36280dd
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-26 14:11:21 +0900

    chore: VotesListPageVotes 컴포넌트 수정

--- src/components/VoteListPageVotes.tsx
+++ src/components/VoteListPageVotes.tsx
@@ -4,7 +4,7 @@ import { VoteCard } from "@/components/ui/VoteCard";
 import { VoteCardSkeleton } from "@/components/ui/VoteCardSekeleton";
 import { type CategoriesValue } from "@/const/const";
 import { useGetAllVotesByAsc, useGetAllVotesByDesc, useGetPopularVotes } from "@/hooks/useGetVotes";
-import { useDatetSortSelectStore, useFilterStore } from "@/libs/zustand/store";
+import { useDatetSortSelectStore, useFilterStore, useSearchStore } from "@/libs/zustand/store";
 
 import { useEffect, useMemo } from "react";
 
@@ -25,6 +25,8 @@ const useSortedVotes = () => {
 };
 
 export const VoteListPageVotes = () => {
+  const { search } = useSearchStore();
+
   const { votes, isLoading, isError } = useSortedVotes();
   const { filter, filterApplied } = useFilterStore();
 
@@ -34,9 +36,19 @@ export const VoteListPageVotes = () => {
     // eslint-disable-next-line
   }, [filterApplied, votes]);
 
+  const searchedVotes = useMemo(() => {
+    return filteredVotes?.filter((vote) =>
+      vote.title
+        .toLowerCase()
+        .trim()
+        .replaceAll(" ", "")
+        .includes(search.toLowerCase().trim().replaceAll(" ", ""))
+    );
+  }, [search, filteredVotes]);
+
   useEffect(() => {
-    console.log(filterApplied);
-  }, [filterApplied]);
+    console.log(search);
+  }, [search]);
 
   if (isError) throw new Error("Error: VoteListPageVotes 컴포넌트");
 
@@ -47,12 +59,20 @@ export const VoteListPageVotes = () => {
       <div className="vote-grid grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {isLoading
           ? Array.from({ length: 15 }).map((_, idx) => <VoteCardSkeleton key={idx} />)
-          : filteredVotes?.map((vote) => (
-              <VoteCard
-                key={vote.id}
-                vote={vote}
-              />
-            ))}
+          : search
+            ? searchedVotes?.map((vote) => (
+                <VoteCard
+                  key={vote.id}
+                  vote={vote}
+                />
+              ))
+            : filteredVotes?.map((vote) => (
+                <VoteCard
+                  key={vote.id}
+                  vote={vote}
+                />
+              ))}
+        {searchedVotes?.length === 0 && <p>검색된 투표가 없어요.</p>}
       </div>
     </section>
   );
