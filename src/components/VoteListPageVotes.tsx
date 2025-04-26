e62385c2e4b2a8fc3d5dbc30d6931a7d2c1afbf6
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-26 09:45:51 +0900

    chore: VoteListPageVotes 컴포넌트 추가

---
+++ src/components/VoteListPageVotes.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { VoteCard } from "@/components/ui/VoteCard";
+import { VoteCardSkeleton } from "@/components/ui/VoteCardSekeleton";
+import { useGetAllVotesByAsc, useGetAllVotesByDesc, useGetPopularVotes } from "@/hooks/useGetVotes";
+import { useDatetSortSelectStore } from "@/libs/zustand/store";
+
+const useSortedVotes = () => {
+  const { dateSelect } = useDatetSortSelectStore();
+
+  const latest = useGetAllVotesByDesc();
+  const oldest = useGetAllVotesByAsc();
+  const popular = useGetPopularVotes();
+
+  const isLoading = latest.isLoading || oldest.isLoading || popular.isLoading;
+  const isError = latest.isError || oldest.isError || popular.isError;
+
+  const votes =
+    dateSelect === "latest" ? latest.data : dateSelect === "oldest" ? oldest.data : popular.data;
+
+  return { votes, isLoading, isError };
+};
+
+export const VoteListPageVotes = () => {
+  const { votes, isLoading, isError } = useSortedVotes();
+
+  if (isError) throw new Error("Error: VoteListPageVotes 컴포넌트");
+
+  if (!votes) return;
+
+  return (
+    <section className="mx-auto w-full space-y-4">
+      <div className="vote-grid grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {isLoading
+          ? Array.from({ length: 15 }).map((_, idx) => <VoteCardSkeleton key={idx} />)
+          : votes.map((vote) => (
+              <VoteCard
+                key={vote.id}
+                vote={vote}
+              />
+            ))}
+      </div>
+    </section>
+  );
+};
