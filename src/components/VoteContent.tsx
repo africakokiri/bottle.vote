0e5e26c184f4c9bf280e7134295a93e167d9683d
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-27 14:25:08 +0900

    chore: VoteContent 컴포넌트 수정

--- src/components/VoteContent.tsx
+++ src/components/VoteContent.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { Button } from "@/components/ui/Button";
-import { type vote_options } from "@/generated/prisma";
+import type { Prisma, vote_options, votes } from "@/generated/prisma";
 import { cn } from "@/utils/cn";
 
 import { motion } from "motion/react";
 import { type Dispatch, type FormEvent, type SetStateAction, useState } from "react";
+
+type VoteWithOption = Prisma.votesGetPayload<{
+  include: {
+    vote_options: true;
+  };
+}>;
 
 export const Votes = ({
   voteOptions,
@@ -45,7 +51,7 @@ export const Votes = ({
               htmlFor={id.toString()}
               className={cn(
                 `bg-element flex flex-1 appearance-none items-center gap-2 rounded-lg border p-4
-text-left accent-fuchsia-500 transition hover:border-fuchsia-500`,
+text-left accent-fuchsia-500 hover:border-fuchsia-500 sm:transition`,
                 checkedRadio === name && "border-fuchsia-500 bg-fuchsia-100",
                 checkedCheckbox.includes(name) && "border-fuchsia-500 bg-fuchsia-100"
               )}
@@ -75,13 +81,40 @@ text-left accent-fuchsia-500 transition hover:border-fuchsia-500`,
   );
 };
 
-export const VoteContent = ({
-  voteOptions,
-  isDuplicateVote
-}: {
-  voteOptions: vote_options[];
-  isDuplicateVote: boolean;
-}) => {
+const VoteResult = ({ vote }: { vote: VoteWithOption }) => {
+  return (
+    <div className="space-y-4">
+      <h3 className="text-lg font-medium">현재 결과</h3>
+
+      <div className="space-y-4">
+        {vote.vote_options.map((option) => (
+          <div
+            key={option.id}
+            className="space-y-2"
+          >
+            <div className="flex justify-between text-sm">
+              <span>{option.name}</span>
+              <div className="flex gap-3">
+                <span>{option.vote_count.toLocaleString()}표</span>
+                <span className="font-medium">
+                  {Math.round((option.vote_count / vote.vote_count) * 100)}%
+                </span>
+              </div>
+            </div>
+            <div className="h-4 overflow-hidden rounded-full bg-neutral-200 dark:bg-neutral-700">
+              <div
+                className="gradient-bg h-full"
+                style={{ width: `${(option.vote_count / vote.vote_count) * 100}%` }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const VoteContent = ({ vote }: { vote: VoteWithOption }) => {
   const [type, setType] = useState("투표");
   const [checkedRadio, setCheckedRadio] = useState("");
   const [checkedCheckbox, setCheckedCheckbox] = useState<string[]>([]);
@@ -129,15 +162,17 @@ export const VoteContent = ({
         onSubmit={handleOnSubmit}
         className="flex flex-col space-y-4"
       >
-        {type === "투표" && (
+        {type === "투표" ? (
           <Votes
-            voteOptions={voteOptions}
-            isDuplicateVote={isDuplicateVote}
+            voteOptions={vote.vote_options}
+            isDuplicateVote={vote.is_duplicate_vote}
             checkedRadio={checkedRadio}
             checkedCheckbox={checkedCheckbox}
             setCheckedRadio={setCheckedRadio}
             setCheckedCheckbox={setCheckedCheckbox}
           />
+        ) : (
+          <VoteResult vote={vote} />
         )}
       </form>
     </div>
