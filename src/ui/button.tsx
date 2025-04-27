76bbb60e93583e72826f7eb34470bfba38f97e2d
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-27 16:13:17 +0900

    chore: Button UI 컴포넌트 추가

---
+++ src/ui/button.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/utils/tailwind/cn";
+
+import { type ButtonHTMLAttributes } from "react";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+  className?: string;
+  variant: "default" | "icon";
+  ref?: React.Ref<HTMLButtonElement>;
+};
+
+export const Button = ({ children, className, variant, ref, ...props }: ButtonProps) => {
+  let buttonVariant;
+
+  if (variant === "icon") buttonVariant = "p-3 border-none";
+
+  return (
+    <button
+      ref={ref}
+      className={cn(
+        `bg-element min-w-fit cursor-pointer rounded-lg px-4 py-2 font-semibold hover:bg-neutral-200
+dark:hover:bg-neutral-700`,
+        className,
+        buttonVariant
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
